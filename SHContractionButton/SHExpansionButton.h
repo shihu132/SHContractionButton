588f20e12d4481a4777546e29d5a95445495eecd
shihu <13261089216@163.com>
2018-09-28 11:57:17 +0800

    点击 button 横向展开收缩和竖向展开和收缩

---
+++ SHContractionButton/SHExpansionButton.h
@@ -0,0 +1,13 @@
+//
+//  SHExpansionButton.h
+//  SHContractionButton
+//
+//  Created by joyshow on 2018/9/28.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHExpansionButton : UIButton
+
+@end
---
+++ SHContractionButton/SHExpansionButton.m
@@ -0,0 +1,21 @@
+//
+//  SHExpansionButton.m
+//  SHContractionButton
+//
+//  Created by joyshow on 2018/9/28.
+//  Copyright © 2018年 石虎. All rights reserved.
+//
+
+#import "SHExpansionButton.h"
+
+@implementation SHExpansionButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
